6bfd0f84f926acb4d4ea0dc819198dc6ad7d51cb
unknown <sarniha12@gmail.com>
2026-07-12 12:55:38 +0530

    new functions written

--- db_engine/src/main.rs
+++ db_engine/src/main.rs
@@ -6,8 +6,12 @@ fn main() {
     set_page_id(&mut p, 42);
     set_slot_count(&mut p, 0);
     set_free_space_start(&mut p, 4096);
+    set_slot_offset(&mut p,2,26);
 
     println!("page_id: {}", get_page_id(&p));
     println!("slot_count: {}", get_slot_count(&p));
     println!("free_space_start: {}", get_free_space_start(&p));
+    println!("slot{}",get_slot_offset(&p,2));
+    set_slot_length(&mut p, 2, 50);
+println!("slot 2 length: {}", get_slot_length(&p, 2));
 }
--- db_engine/src/page.rs
+++ db_engine/src/page.rs
@@ -2,6 +2,8 @@ pub struct Page {
     pub data: [u8; 4096],
 }
 
+//Header functions
+
 pub fn get_page_id(page: &Page) -> u16 {
     u16::from_le_bytes(page.data[0..2].try_into().unwrap())
 }
@@ -9,6 +11,7 @@ pub fn get_page_id(page: &Page) -> u16 {
 pub fn set_page_id(page: &mut Page, id: u16) {
     page.data[0..2].copy_from_slice(&id.to_le_bytes());
 }
+//page.data gets copied data brought by to_le_bytes
 
 pub fn get_slot_count(page: &Page) -> u16 {
     u16::from_le_bytes(page.data[2..4].try_into().unwrap())
@@ -24,4 +27,24 @@ pub fn get_free_space_start(page: &Page) -> u16 {
 
 pub fn set_free_space_start(page: &mut Page, offset: u16) {
     page.data[4..6].copy_from_slice(&offset.to_le_bytes());
+}
+
+pub fn get_slot_offset(page: &Page, slot_id: u16) -> u16 {
+    let start = 6 + (slot_id * 4) as usize;
+    u16::from_le_bytes(page.data[start..start+2].try_into().unwrap())
+}
+
+pub fn set_slot_offset(page: &mut Page,slot_id:u16,offset: u16){
+    let start=6+(slot_id*4) as usize;
+    page.data[start..start+2].copy_from_slice(&offset.to_le_bytes());
+
+
+}
+pub fn set_slot_length(page: &mut Page,slot_id:u16,length: u16){
+    let start=6+(slot_id*4) as usize;
+    page.data[start+2..start+4].copy_from_slice(&length.to_le_bytes());
+}
+pub fn get_slot_length(page: &Page, slot_id: u16) -> u16 {
+    let start = 6 + (slot_id * 4) as usize;
+    u16::from_le_bytes(page.data[start+2..start+4].try_into().unwrap())
 }
